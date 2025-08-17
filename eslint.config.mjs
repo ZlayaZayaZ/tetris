cfcc6f304fbbf59d208f95b052585f080b5b72f8
Vasilkova Lyubov <damirana_@mail.ru>
2025-08-18 01:15:54 +0700

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,vue}"], ignores: ['dist/**', 'node_modules/**', 'docs/**', 'coverage/**', 'webpack.config.js'], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginVue.configs["flat/essential"],
 ]);
